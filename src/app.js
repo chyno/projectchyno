2374c72538d3e356a595bffdaa173824f0783989
jxc14 <John.Chynoweth-contractor@nreca.coop>
2016-10-11 14:14:03 -0400

    navigation works

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Project Chyno';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title: 'Welcome' }
+      { route: ['', 'welcome'], name: 'welcome', moduleId: './welcome', nav: true, title: 'Welcome' },
+       { route: ['kota', 'kota'], name: 'kota',      moduleId: './kota',      nav: true, title: 'Kota' }
      /*
       { route: 'users', name: 'users', moduleId: './users', nav: true, title: 'Github Users' },
       { route: 'child-router',  name: 'child-router', moduleId: './child-router', nav: true, title: 'Child Router' }
